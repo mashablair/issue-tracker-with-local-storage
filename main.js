c81090d650d54aa875b2915bee0896a31cee58c6
Masha Blair <mashablair@gmail.com>
2019-09-05 17:26:32 -0700

    Fix bug of initial state where issues in localStorage is null.

--- main.js
+++ main.js
@@ -8,40 +8,42 @@ function fetchIssues() {
 
   issuesList.innerHTML = "";
 
-  if (issues.length > 0) {
-    for (var i = 0; i < issues.length; i++) {
-      var id = issues[i].id;
-      var desc = issues[i].description;
-      var severity = issues[i].severity;
-      var assignedTo = issues[i].assignedTo;
-      var status = issues[i].status;
+  if (issues) {
+    if (issues.length > 0) {
+      for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
 
-      issuesList.innerHTML +=
-        '<div class="well">' +
-        "<h6>Issue ID: " +
-        id +
-        "</h6>" +
-        '<p><span class="label label-info">' +
-        status +
-        "</span></p>" +
-        "<h3>" +
-        desc +
-        "</h3>" +
-        '<p><span class="glyphicon glyphicon-time"></span> ' +
-        severity +
-        " " +
-        '<span class="glyphicon glyphicon-user"></span> ' +
-        assignedTo +
-        "</p>" +
-        // Close btn
-        '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' +
-        id +
-        "')\">Close</a> " +
-        // Delete btn
-        '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' +
-        id +
-        "')\">Delete</a>" +
-        "</div>";
+        issuesList.innerHTML +=
+          '<div class="well">' +
+          "<h6>Issue ID: " +
+          id +
+          "</h6>" +
+          '<p><span class="label label-info">' +
+          status +
+          "</span></p>" +
+          "<h3>" +
+          desc +
+          "</h3>" +
+          '<p><span class="glyphicon glyphicon-time"></span> ' +
+          severity +
+          " " +
+          '<span class="glyphicon glyphicon-user"></span> ' +
+          assignedTo +
+          "</p>" +
+          // Close btn
+          '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' +
+          id +
+          "')\">Close</a> " +
+          // Delete btn
+          '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' +
+          id +
+          "')\">Delete</a>" +
+          "</div>";
+      }
     }
   }
 }
